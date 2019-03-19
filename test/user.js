e69e255ed983bd963b07d4291a57a6d6da8ff65f
Copain Fabrice  BIENAIME <copainfabricebienaime@Dr-Copain.local>
2019-03-19 12:26:24 +0200

    chore(Added test): Added test environment
    - testing environment added using mocha & chai
    
    [Delivers #164489920]

---
+++ test/user.js
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index'
+
+chai.use(chaiHttp);
+chai.should()
+
+describe('GET INVALID_URL', () =>{
+    it('should return an error', (done) =>{
+        chai.request(app)
+        .get('/api/v1/INVALID_URL')
+        .end((err, res) =>{
+            res.should.have.status(404);
+            done();
+        })
+    })
+});
