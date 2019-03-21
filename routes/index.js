7fccb00a38e10210f9389ad482a7cd32b685e33c
Itsgracian <gracian2020@gmail.oom>
2019-03-21 14:24:44 +0200

    chore(setup): setup API documentation with swagger

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../config/swagger.json';
 
-//@api
-
-
-//@ initialize app
-
+// @api
+// @ initialize app
 const app = express();
 
-//@ add package configurations
-
+// @ add package configurations
+// @swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
