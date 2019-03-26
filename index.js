b2cc13920d6576fc9b1c5e2037fa43e62daff905
Itsgracian <gracian2020@gmail.oom>
2019-03-26 11:44:33 +0200

    mend

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import http from 'http';
-import app from './routes/index';
-
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import bodyParser from "body-parser";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './config/swagger.json';
+import router from "./routes/index";
+const app = express();
 const port = process.env.PORT || 3000;
+app.set('port', port);
 
-app.set('poort', port);
+//@bodyParser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+// @ add package configurations
+// @swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//@router configuration --gracian
+app.use(router);
 
-const server = http.createServer(app);
+app.use((req, res) => {
+  res.status(404).send({
+    status: 404,
+    error: 'resource not found',
+  });
+});
+app.listen(port, () => {
+  console.log(`Server started successfully on ${port}`);
+});
 
-server.listen(port, () =>{
-    console.log(`Server started successfully on ${port}`);
-})
+export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import bodyParser from "body-parser";
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../config/swagger.json';
-
-
-//@router 
 import user from "./api/users";
-
-// @api
-// @ initialize app
-const app = express();
-
-//@bodyParser configuration
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-// @ add package configurations
-// @swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+//@create express router
+const app = express.Router();
 //@router configuration
 app.use("/api/v1",user);
 
