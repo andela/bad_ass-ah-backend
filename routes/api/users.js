a565fea4dd03b4b990066e2c76d718f0f0d9b184
P.C. Ndayisenga <paccy@Pacifiques-MacBook-Pro.local>
2019-04-03 12:42:05 +0200

    Feature(login): User login
    -creating login endpoint
    
    [Delivers #1165082475]

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,19 @@
-import { Router } from 'express';
+import express from 'express';
+// @controller
+import User from '../../controllers/user';
+// @middleware
 import check from '../../middlewares/user';
-import user from '../../controllers/user';
 import VerifyLink from '../../controllers/email/verifyLink';
 
-const router = Router();
+const router = express.Router();
 
 // @POST
 // @description creating user
-router.post('/', check, user.signup);
+router.post('/', check, User.signup);
+
 router.post('/send-verification-link', VerifyLink.sendEmail);
 router.get('/verify/:token', VerifyLink.activate);
+router.post('/login', User.login);
+
 
 export default router;
