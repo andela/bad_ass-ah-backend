f5026957cc9a36609e2725b1ddac02dde4269b95
cop1fab <bienaimecopain@gmail.com>
2019-06-07 17:05:43 +0200

    [bug-#166324793] changing the verification link

--- helpers/sendEmail/emailTemplates.js
+++ helpers/sendEmail/emailTemplates.js
@@ -17,7 +17,7 @@ const emailTemplates = {
     subject: 'Email Verification',
     html: `<h1 style="color: #444; margin-left: 20px;">Welcome to Author's Haven</h1>
 <p style="color: #555; margin-left: 20px; font-size: 14px">Thank you for signing to the Author's Haven. Please click on the button below to activate your account.</p><br>
-<a style="background-color: #61a46e; padding: 12px 15px 12px 15px; color: #eee; font-size: 16px; text-decoration: none; margin-left: 20px; cursor: pointer;" href="${clientVerifytUrl}/verify/$token">Activate account</a>`
+<a style="background-color: #61a46e; padding: 12px 15px 12px 15px; color: #eee; font-size: 16px; text-decoration: none; margin-left: 20px; cursor: pointer;" href="${clientVerifytUrl}/login/$token">Activate account</a>`
   },
 
   resetPassword: {
