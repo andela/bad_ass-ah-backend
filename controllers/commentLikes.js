e638c2cb6a23dc8186433b4002e69dba97e2507f
Itsgracian <gracian2020@gmail.oom>
2019-06-10 09:35:34 +0200

    Bug(comment) fix errors comment features

--- controllers/commentLikes.js
+++ controllers/commentLikes.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 import models from '../models/index';
 
 const VoteComment = models.votecomment;
@@ -16,22 +17,18 @@ class CommentVotesController {
    */
   async likeComment(req, res) {
     try {
-      const likeData = {
-        userId: req.user.id,
-        commentId:
-        req.params.commentId,
-        like: true,
-        dislike: false
-      };
+      // eslint-disable-next-line max-len
+      const likeData = { userId: req.user.id, commentId: req.params.commentId, like: true, dislike: false };
       if (req.likeComment === null) {
         await VoteComment.create(likeData);
-        return res.status(200).json({ message: 'thanks for the support.', userId: req.user.id, comment: req.params.commentId });
+        return res.status(200).json({ message: 'thanks for the support.', userId: req.user.id, comment: req.params.commentId, hasLiked: true });
       }
       if (req.likeComment.like === true) {
-        return res.status(400).json({ error: 'sorry you have already liked this comment.' });
+        return res.status(400).json({ error: 'sorry you have already liked this comment.', hasLiked: false });
       }
       await VoteComment.update(likeData, { where: { id: req.likeComment.id } });
-      return res.status(200).json({ message: 'thanks for the support.', userId: req.user.id, comment: req.likeComment });
+      return res.status(200).json({ message: 'thanks for the support.', userId: req.user.id, comment: req.likeComment, hasLiked: true
+      });
     } catch (error) {
       return res.status(500).json({ error: 'something wrong try again later.' });
     }
@@ -46,23 +43,23 @@ class CommentVotesController {
   // eslint-disable-next-line require-jsdoc
   async dislikeComment(req, res) {
     try {
-      const dislikeData = {
-        userId: req.user.id,
-        commentId: req.params.commentId,
-        like: false,
-        dislike: true
-      };
+    // eslint-disable-next-line max-len
+      const dislikeData = { userId: req.user.id, commentId: req.params.commentId, like: false, dislike: true };
       if (req.likeComment === null) {
         await VoteComment.create(dislikeData);
-        return res.status(200).json({ message: 'thank for support.' });
+        return res.status(200).json({ message: 'thanks.', hasDisLiked: true });
       }
       if (req.likeComment.dislike === true) {
-        return res.status(400).json({ error: 'sorry you have already disliked this comment.' });
+        return res.status(400).json({
+          error: 'sorry you have already disliked this comment.',
+          hasDisLiked: false
+        });
       }
       await VoteComment.update(dislikeData, {
         where: { id: req.likeComment.id }
       });
-      return res.status(200).json({ message: 'You have disliked this article.', userId: req.user.id, commentId: req.params.commentId, });
+      return res.status(200).json({ message: 'thanks.', userId: req.user.id, commentId: req.params.commentId, hasDisLiked: true
+      });
     } catch (error) {
       return res.status(500).json({ error: 'something wrong try again later.' });
     }
